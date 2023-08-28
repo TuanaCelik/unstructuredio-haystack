b21381ec133d352a6c804e52e9c01f9608459961
Tuana Celik <tuana.celik@deepset.ai>
2023-08-28 19:23:21 +0200

    init and about

---
+++ src/unstructured_haystack/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Tuana Celik <tuana.celik@deepset.ai>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/unstructured_haystack/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present Tuana Celik <tuana.celik@deepset.ai>
+#
+# SPDX-License-Identifier: MIT
+
+from .unstuctured_haystack import UnstructuredDiscordConnector
+from .unstuctured_haystack import UnstructuredGitHubConnector
