54ef7f8c7630d011b96163bc239645689c66de91
Tuana Celik <tuana.celik@deepset.ai>
2023-08-29 23:50:16 +0200

    base class

--- src/unstructured_haystack/unstuctured_haystack.py
+++ src/unstructured_haystack/unstuctured_haystack.py
@@ -1,101 +1,106 @@
 import subprocess
+import os
 from typing import Dict, List, Any, Optional
 from canals.serialization import default_to_dict, default_from_dict
 from haystack.preview import component, Document
+import glob, json
 
 @component
-class UnstructuredGitHubConnector:
-    """
-    A component that allows you to use Unstrucrured.io connectors to fetch Documents from various APIs.
-
-    """
-    def __init__(self, api_key: str = None):
-        
-        self.command = [
-        "unstructured-ingest",
-        "github",
-        "--api-key", api_key,
-        ]
-        self.api_key = api_key
-
+class UnstructuredConnector:
+    def __init__(self, api_key: str, output_dir: str):
+        self.args = ["unstructured-ingest"]
+        self.opts = ["--api-key", api_key]
+       
+        self.output_dir = output_dir
+    
     def to_dict(self) -> Dict[str, Any]:
-        return default_to_dict(self, connector=self.connector, api_key=self.api_key)
+        return default_to_dict(self, args=self.args, opts = self.opts, output_dir=self.output_dir)
     
     @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "UnstructuredGitHubConnector":
+    def from_dict(cls, data: Dict[str, Any]) -> "UnstructuredConnector":
         return default_from_dict(cls, data)
     
-    @component.output_types(documents=List[List[Document]])
-    def run(self, url: str, git_branch: str = "main", num_processes: int = 2):
-         # Run the command
-        options = [
-        "--url", url,
-        "--git-branch", git_branch,
-        "--structured-output-dir",  "github-ingest-output",
-        "--num-processes", str(num_processes),
-        "--verbose",
-        "--partition-by-api"]
-        
-        self.command.extend(options)
-
-        process = subprocess.Popen(self.command, stdout=subprocess.PIPE)
+    def run(self):
+        command = self.args + self.opts
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
         output, error = process.communicate()
 
         # Print output
         if process.returncode == 0:
-            print('Command executed successfully. Output:')
-            print(output.decode())
+            haystack_docs=[]
+
+            for json_file in glob.glob('{self.output_dir}/**/*.json', recursive=True):
+                with open(json_file,'r') as fin:
+                    unstructured_doc = json.load(fin)
+                for el in unstructured_doc:
+                    text = "\n".join(el['text'] for el in unstructured_doc)
+                    haystack_docs.append(Document(content=text, metadata=el['metadata']))
         else:
             print('Command failed. Error:')
             print(error.decode())
-        return [Document(content="blablabla")]
-
-
+        return haystack_docs
+    
 @component
-class UnstructuredDiscordConnector:
+class UnstructuredGitHubConnector(UnstructuredConnector):
     """
     A component that allows you to use Unstrucrured.io connectors to fetch Documents from various APIs.
 
     """
-    def __init__(self, api_key: str = None, discord_token: str = None):
-        
-        self.command = [
-        "unstructured-ingest",
-        "discord",
-        "--token", discord_token,
-        "--api-key", api_key,
-        ]
-        self.api_key = api_key
-        self.discord_token = discord_token
+    def __init__(self, api_key: str, repo: str, git_branch: str = "main", output_dir: str = "github-ingest-output"):
+        super().__init__(api_key=api_key, output_dir = output_dir)
 
-    def to_dict(self) -> Dict[str, Any]:
-        return default_to_dict(self, discord_token=self.discord_token, api_key=self.api_key)
-    
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "UnstructuredDiscordConnector":
-        return default_from_dict(cls, data)
+        self.args.append("github")
+        self.repo = repo
+        self.git_branch = git_branch
+        self.output_dir = output_dir
+       
     
     @component.output_types(documents=List[List[Document]])
-    def run(self, channels: str = None, download_dir: str = "discord-ingest-download"):
+    def run(self, repo: Optional[str] = None, git_branch: Optional[str] = None):
          # Run the command
-        options = [
+        self.opts.append("--url")
+        if repo !=  None:
+            self.opts.append(repo)
+        else:
+            self.opts.append(self.repo)
+        
+        self.opts.append("--git-branch")
+        if git_branch !=  None:
+            self.opts.append(git_branch)
+        else:
+            self.opts.append(self.git_branch)
+        
+        self.opts.extend([
+        "--structured-output-dir",  self.output_dir,
+        "--num-processes", "2",
+        "--verbose",
+        "--partition-by-api"
+        ])
+        
+        return super().run()
+
+
+@component
+class UnstructuredDiscordConnector(UnstructuredConnector):
+    """
+    A component that allows you to use Unstrucrured.io connectors to fetch Documents from various APIs.
+
+    """
+    def __init__(self, api_key: str, discord_token: str, output_dir: str = "discord-example"):
+        super().__init__(api_key=api_key, output_dir=output_dir)
+        self.args.append("discord")
+        self.opts.extend(["--token", discord_token])
+    
+    @component.output_types(documents=List[List[Document]])
+    def run(self, channels: str, output_dir: str = "discord-example", period: int = 1):
+         # Run the command
+        self.opts.extend([
         "--channels", channels,
-        "--download-dir", download_dir,
-        "--structured-output-dir", "discord-example",
+        "--download-dir", "discord-ingest-download",
+        "--structured-output-dir", output_dir,
+        "--period", str(period),
         "--preserve-downloads",
         "--verbose",
-        "--partition-by-api"]
+        "--partition-by-api"])
         
-        self.command.extend(options)
-
-        process = subprocess.Popen(self.command, stdout=subprocess.PIPE)
-        output, error = process.communicate()
-
-        # Print output
-        if process.returncode == 0:
-            print('Command executed successfully. Output:')
-            print(output.decode())
-        else:
-            print('Command failed. Error:')
-            print(error.decode())
-        return [Document(content="blablabla")]
+        return super().run()
